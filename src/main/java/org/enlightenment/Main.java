f79f078a77afe367617ddb1a21dfeeea89497fb2
aleks <alekswickey.ost26@gmail.com>
2025-02-09 15:00:41 +0200

    beb

--- src/main/java/org/enlightenment/Main.java
+++ src/main/java/org/enlightenment/Main.java
@@ -1,7 +1,32 @@
 package org.enlightenment;
 
+//player is always first
+//player always puts X
+//console always puts 0
+//console chooses a random empty spot
+//no OOP
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static void gameLoop() {
+        //while (game not over)
+        //playerTurn
+        //consoleTurn
+        //check Victory state
+    }
+
+    public static void playerTurn() {
+        // get input
+        // validate input
+        // place X on board
+    }
+
+    public static void consoleTurn() {
+        // get random empty cell
+        // place 0 on board
+    }
+
 }
